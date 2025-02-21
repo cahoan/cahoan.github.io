73f2b8af7807102929e6f7867465fa9c9514b0dd
cahoan <88755923+cahoan@users.noreply.github.com>
2025-02-21 16:50:22 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -616,7 +616,7 @@ function updateVoices() {
         if (!isAlreadyAdded) {
             const option = new Option(voice.name, voice.voiceURI, voice.default, voice.default);
             //if ((option.value.search("English")>0) || (option.value.search("en-US")>0) || (option.value.search("Linh")>0) || (option.value.search("An")>0) || (option.value.search("vi-VN")>0)){
-            if ( (option.value.search("Linh")>0) || (option.value.search("Samantha")>0) || ((option.value.search("An")>0 && option.value.search("Anna")<0)) || (option.value.search("Zira")>0) || (option.value.search("Linh")>=0) ){
+            if ( (option.value.search("Samantha")>0) || ((option.value.search("An")>0 && option.value.search("Anna")<0)) || (option.value.search("Zira")>0)  ){
                 //console.log(option);
                 if (option.value.search("Samantha")>=0){
                   option.selected=true;
